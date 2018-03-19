703e46d9f9eb242a7bb8542b30a9ffe709548fef
Nai <aksh.nai@stu.jefferson.kyschools.us>
2018-03-19 08:22:29 -0400

    my dictionary work

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+my_dictionary = {
+    
+    "grudge" : "a presistent feeling of ill will or resentment resulting from a [ast insult or injury",
+    
+    "detectable" : "able to be discovered or identified",
+    
+    "infantryman" : "a soldier belonging to an infantry unit",
+    
+    "drown" : "die through submersion in and inhalation of water",
+    
+    "gluten"  : "a substance present in cereal grains, especially wheat, that is responsible for the elastic texture of dough. A mixture of two proteins, it causes illness in people with celiac disease",
+    
+    "demagogue" : "a political leader who seeks support by appealing to popular desires and prejudices rather than by using rational argument",
+    
+    "tentative" : "not certain or fixed,provisional",
+    
+    "mediate" : "intervene between people in a dispute in order to bring about an agreement or reconciliation",
+    
+    }
